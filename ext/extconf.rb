596b03e6d5dbf8ebaa0dc970e532da6a9544d1d9
Ryan Fitzgerald <rwfitzge@gmail.com>
2012-11-23 15:55:41 -0800

    Fix extconf bug

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,7 @@ dir_config("opt", "/opt/local/include",  "/opt/local/lib")
 have_header("ruby.h")
 
 unless have_header("aspell.h")
-  $CPPFLAGS += "-D NO_ASPELL"
+  $CPPFLAGS += " -D NO_ASPELL"
 end
 
 have_library("aspell")
